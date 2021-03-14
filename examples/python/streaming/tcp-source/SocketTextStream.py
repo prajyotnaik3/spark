b1683f3248d42355a2aacc108dd7fc5791d6ef6f
prajyotnaik3 <56463673+prajyotnaik3@users.noreply.github.com>
2021-03-14 17:42:33 +0530

    Update SocketTextStream.py

--- examples/python/streaming/tcp-source/SocketTextStream.py
+++ examples/python/streaming/tcp-source/SocketTextStream.py
@@ -1,12 +1,18 @@
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 
+#Create a local StreamingContext with two working thread and batch interval of 5 seconds
 sc = SparkContext("local[2]", "SocketTextStream")
 ssc = StreamingContext(sc, 5)
 
+#Create a DStream that will connect to hostname:port, like localhost:9999
 lines = ssc.socketTextStream("localhost", 9999)
 
+#Print the lines 
 lines.pprint()
 
+#Start the computation
 ssc.start()
+
+#Wait for the computation to terminate
 ssc.awaitTermination()
