5ca91e701a35c89cf9f7b808642a5bde318e5fd2
prajyotnaik3 <56463673+prajyotnaik3@users.noreply.github.com>
2021-03-15 18:49:34 +0530

    Update DataFrameCreation.py
    Added creating dataframe from RDD using (1) reflection and (2) programmatically specified schema.

--- examples/python/dataframe-operations/DataFrameCreation.py
+++ examples/python/dataframe-operations/DataFrameCreation.py
@@ -1,6 +1,6 @@
-from pyspark import SparkContext
-from pyspark.sql import SQLContext
-from pyspark import HiveContext
+from pyspark import SparkContext, HiveContext
+from pyspark.sql import SQLContext, Row
+from pyspark.sql.types import *
 
 sc = SparkContext("local[2]")
 
@@ -10,14 +10,12 @@ sqlContext = SQLContext(sc)
 df1 = sqlContext.read.csv("hdfs://cxln1.c.thelab-240901.internal:8020/user/prajyotnaikxxxxx/sparkData/iris.csv", header = True, inferSchema = True)
 
 df1.printSchema()
-
 df1.show()
 
 #Creating dataframe from a json file
 df2 = sqlContext.read.json("hdfs://cxln1.c.thelab-240901.internal:8020/user/prajyotnaikxxxxx/sparkData/employee.json")
 
 df2.printSchema()
-
 df2.show()
 
 #Creating dataframe using a hive table
@@ -26,5 +24,23 @@ hqlContext = HiveContext(sc)
 df3 = hqlContext.sql("select * from prajyot.employee")
 
 df3.printSchema()
-
 df3.show()
+
+#Creating dataframe from RDD
+lines = sc.textFile("hdfs://cxln1.c.thelab-240901.internal:8020/user/prajyotnaik37389/sparkData/employee.txt")
+parts = lines.map(lambda line: line.split("\t"))
+
+#By inferring the schema using Reflection
+employee = parts.map(lambda e: Row(id = int(e[0]), name = e[1], age = int(e[2])))
+df4 = sqlContext.createDataFrame(employee)
+
+df4.printSchema()
+df4.show()
+
+#By programmatically specifying the schema
+employee2 = parts.map(lambda e: (int(e[0]), e[1].strip(), int(e[2])))
+schema = StructType([StructField("id", IntegerType(), True), StructField("name", StringType(), True), StructField("age", IntegerType(), True)])
+df5 = sqlContext.createDataFrame(employee2, schema)
+
+df5.printSchema()
+df5.show()
